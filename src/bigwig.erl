26b3868c616e68dbd0abc9cbc863853f89da5123
root <root@chat.dev0.spark.net>
2013-06-19 14:25:04 -0400

    merge

--- src/bigwig.erl
+++ src/bigwig.erl
@@ -2,7 +2,16 @@
 -export([start/0, stop/0]).
 
 start() ->
+<<<<<<< HEAD
     start(bigwig).
+=======
+    ensure_started(crypto),
+    ensure_started(sasl),
+    ensure_started(cowboy),
+    ensure_started(jsx),
+    application:start(bigwig).
+
+>>>>>>> 2aa43a5... add ensure started jsx
 stop() ->
     application:stop(bigwig).
 start(App) ->
