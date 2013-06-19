ce0c3cb6592da230e907d16e1e454a5a59c00cc0
root <root@chat.dev0.spark.net>
2013-06-19 14:27:24 -0400

    ensure jsx start and pattern match to show which app fail to start

--- src/bigwig.erl
+++ src/bigwig.erl
@@ -2,16 +2,13 @@
 -export([start/0, stop/0]).
 
 start() ->
-<<<<<<< HEAD
-    start(bigwig).
-=======
-    ensure_started(crypto),
-    ensure_started(sasl),
-    ensure_started(cowboy),
-    ensure_started(jsx),
-    application:start(bigwig).
+    ok = start(crypto),
+    ok = start(sasl),
+    ok = start(cowboy),
+    ok = start(jsx),
+    ok = start(bigwig),
+    ok.
 
->>>>>>> 2aa43a5... add ensure started jsx
 stop() ->
     application:stop(bigwig).
 start(App) ->
